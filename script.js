7ab800a3d61571d65358c4ba87e9e4354e22051b
CandaceAndrews <candacewolfandrews@hotmail.com>
2024-05-02 22:17:24 -0400

    starting on script, sort card and flip card

---
+++ script.js
@@ -0,0 +1,8 @@
+// Array of symbols for the cards
+const symbols = ['🍎', '🍌', '🍉', '🍇', '🍒', '🍍', '🍋', '🍓'];
+
+// Duplicate the symbols to create pairs
+const cards = ['🍎', '🍌', '🍉', '🍇', '🍒', '🍍', '🍋', '🍓'];
+
+// Shuffle the cards
+cards.sort(() => Math.random() - 0.5);
